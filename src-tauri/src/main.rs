81076f8dc560017cc00ae770bd715480d67ec510
Chris Gemmell <fablekey@gmail.com>
2022-05-23 11:00:04 +1000

    changed permissions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,11 @@
   windows_subsystem = "windows"
 )]
 
+use std::fs;
+
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
