55082c55a28fe77a19713e233d3702274bb2a56d
Chris Gemmell <fablekey@gmail.com>
2022-05-25 00:10:51 +1000

    App config files

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import InitialSettings from './components/InitialSettings'
 import './App.css';
 import StudentList from './components/StudentList'
+import { Stack } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
+      <Stack component="form" noValidate spacing={2}>
+        <InitialSettings />
+      </Stack>
       <StudentList />
     </div>
   );
