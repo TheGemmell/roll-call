d3be0b7c9ed9e1a15263abe898ded6d3a38725e3
Chris Gemmell <fablekey@gmail.com>
2022-05-25 18:55:31 +1000

    initalised state in the App level

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,18 @@
-import InitialSettings from './components/InitialSettings'
+import { useState } from 'react'
 import './App.css';
+import InitialSettings from './components/InitialSettings'
 import StudentList from './components/StudentList'
-import { Stack } from '@mui/material';
 
 function App() {
+  const todaysDate = new Date().toISOString().split("T")[0]
+
+  const [ file, setFile ] = useState<string>()
+  const [ date, setDate ] = useState(todaysDate)
+
   return (
     <div className="App">
-      <Stack component="form" noValidate spacing={2}>
-        <InitialSettings />
-      </Stack>
-      <StudentList />
+        <InitialSettings date={date} setDate={setDate} setFile={setFile}/>
+      {/* <StudentList /> */}
     </div>
   );
 }
