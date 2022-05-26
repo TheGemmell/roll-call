b6fd572e8cb3fbdaf715c36e16346ec8f91f1a43
Chris <fablekey@gmail.com>
2022-05-26 23:23:18 +1000

    added menu items interface

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,19 @@
   windows_subsystem = "windows"
 )]
 
+
+
 fn main() {
-  tauri::Builder::default()
+  use tauri::{CustomMenuItem, Menu, Submenu};
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_submenu(submenu)
+    .add_item(CustomMenuItem::new("hide", "Hide"));
+
+    tauri::Builder::default()
+    .menu(menu)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
