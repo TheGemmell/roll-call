21801c04c3fd78959d2fe7567bddd72d211ecf07
Chris <fablekey@gmail.com>
2022-06-13 20:00:51 +1000

    experimenting with about itemmenu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,15 +5,42 @@
 
 
 
-fn main() {
-  use tauri::{CustomMenuItem, Menu, Submenu};
-  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-  let close = CustomMenuItem::new("close".to_string(), "Close");
-  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-  let menu = Menu::new()
-    .add_submenu(submenu)
-    .add_item(CustomMenuItem::new("hide", "Hide"));
 
+
+fn main() {
+  
+  use tauri::{Window, WindowUrl, WindowBuilder};
+  use tauri::{CustomMenuItem, Menu, Submenu, MenuItem, AboutMetadata};
+  // let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  // let close = CustomMenuItem::new("close".to_string(), "Close");
+  // let about = CustomMenuItem::new("about".to_string(), "About");
+  // let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close).add_item(about));
+
+  // let menu = Menu::new()
+  //   .add_submenu(submenu)
+  //   .add_native_item(MenuItem::About("roll_call".to_string(), 
+  //     AboutMetadata::new()
+  //   ))
+  //   .add_item(CustomMenuItem::new("hide", "Hide"));
+  let mut auths = Vec::new();
+  auths.push("Chris".to_string());
+  let ctx = tauri::generate_context!();
+  let name = &ctx.package_info().name;
+  let menu = Menu::new()
+      .add_submenu(Submenu::new(
+          name,
+          Menu::new()
+              .add_native_item(MenuItem::About(
+                  name.to_string(),
+                  AboutMetadata::new().authors(auths)
+              ))
+              .add_native_item(MenuItem::Separator)
+              .add_native_item(MenuItem::Hide)
+              .add_native_item(MenuItem::HideOthers)
+              .add_native_item(MenuItem::ShowAll)
+              .add_native_item(MenuItem::Separator)
+              .add_native_item(MenuItem::Quit),
+      ));
     tauri::Builder::default()
     .menu(menu)
     .run(tauri::generate_context!())
